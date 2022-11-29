64624703027cb1162b941b0b41777693cb04bfe0
Cyril Paquier <cyril.paquier@gmail.com>
2022-11-29 16:26:11 +0100

    js acceuil

---
+++ front/js/script.js
@@ -0,0 +1,21 @@
+let a = fetch(`http://localhost:3000/api/products`)
+    .then(res => res.json())
+    .then(data => {
+        let display = ""
+        for (let article of data) {
+            display += `
+                <a href="./product.html?id=${article._id}">
+                    <article>
+                        <img src="${article.imageUrl}" alt="${article.altTxt}"></img>
+                        <h3 class="productName">${article.name}</h3>
+                        <p class="productDescription">${article.description}</p>
+                    </article>
+                </a>
+                `
+        }
+        console.log(display)
+        document.querySelector("#items").insertAdjacentHTML('beforeend', display)
+    })
+    .catch(err => console.log(er))
+
+console.log(a)
