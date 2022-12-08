02d6c58a4df28b9cc99a36b116e549949899e23d
Cyril Paquier <cyril.paquier@gmail.com>
2022-12-08 15:57:31 +0100

    extraction id

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,17 @@
-/*Récupérer chaine de requête dans l'url*/
-let product_url_id = window.location.search;
-console.log(product_url_id);
+//Récupérer chaine de requête dans l'url
+//let product_url_id = window.location.search;
+//console.log(product_url_id);
+
+//extraine l'ID
+/*let urlSearch = new URLSearchParams(product_url_id);
+let idProduct = URLSearchParams.get(idProduct);
+console.log(id);*/
+
+/*const url = new URL(window.location.href);
+const currentIdProduct = url.searchParams.get("id");
+console.log("id") */
+
+var str = "http://127.0.0.1:5502/front/html/index.html";
+var url = new URL(str);
+var id = url.searchParams.get("id");
+console.log("id");
