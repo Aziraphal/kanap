e077b153a218de0667c92b6f10c201c647349bff
Cyril Paquier <cyril.paquier@gmail.com>
2022-12-08 15:55:26 +0100

    extraction id

--- front/js/script.js
+++ front/js/script.js
@@ -10,26 +10,11 @@ fetch(`http://localhost:3000/api/products`)                 //1. Récupérer les
                 article.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"></img>
                                     <h3 class="productName">${product.name}</h3>
                                     <p class="productDescription">${product.description}</p>`
-            
+
                 a.appendChild(article)                     //3. Injecter dans le DOM
                 document.querySelector("#items").appendChild(a)
-            }
-        
-           // document.querySelector("#items").insertAdjacentHTML('beforeend', display)
-            /*for (let article of data) {
-                display += `
-                    <a href="./product.html?id=${article._id}">
-                        <article>
-                            <img src="${article.imageUrl}" alt="${article.altTxt}"></img>
-                            <h3 class="productName">${article.name}</h3>
-                            <p class="productDescription">${article.description}</p>
-                        </article>
-                    </a>
-                    `
-            }
-            console.log(display)
-            document.querySelector("#items").insertAdjacentHTML('beforeend', display)*/
-        })
-        .catch(er => console.log(er)))
+            }})
+            .catch (er => console.log(er)))
+
 
 
