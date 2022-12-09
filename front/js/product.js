f85898374d4485863f972fe6c6dcffb744be375d
Cyril Paquier <cyril.paquier@gmail.com>
2022-12-09 15:39:27 +0100

    page id product

--- front/js/product.js
+++ front/js/product.js
@@ -4,4 +4,14 @@ console.log(url)
 const currentIdProduct = url.searchParams.get("id");
 console.log(currentIdProduct)
 
+//Affichage de l'objet par son id
+const apiProductsUrl = 'http://localhost:3000/api/products/';
+const productPage = apiProductsUrl + currentIdProduct;
+
+fetch(productPage)
+        .then(r => r.json()
+        .catch((error) => {}))
+
+
+
 
